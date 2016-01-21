b939db0a1fa3d75be1339b666ebcaa4415d8c13c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-01-21 09:01:57 +0100

    Add directive for external links
    Should fix #23

--- www/js/directives.js
+++ www/js/directives.js
@@ -4,7 +4,8 @@
 
     // define the module that will hold all the factories we're gonna use
     angular.module('directives', [])
-      .directive('uboIframeOnload', ['$parse', iframeOnloadDirective]);
+      .directive('uboIframeOnload', ['$parse', iframeOnloadDirective])
+      .directive('uboHref', anchorDirective);
 
     function iframeOnloadDirective($parse) {
         var directive = {
@@ -21,6 +22,35 @@
             });
             element.on('$destroy', function() {
                 element.off('load');
+            });
+        }
+    }
+
+    // Directive to open external links using inAppBrowser
+    // Source: https://forum.ionicframework.com/t/how-to-opening-links-in-content-in-system-browser-instead-of-cordova-browser-wrapper/2427/10
+    function anchorDirective() {
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            var url = '';
+
+            attrs.$observe('uboHref', function(value) {
+                if (value) {
+                    url = value;
+                }
+            });
+
+            element.on('click',function(e){
+                console.log('Opening url:', url);
+                e.preventDefault();
+                if(attrs.ngClick){
+                    scope.$eval(attrs.ngClick);
+                }
+                window.open(encodeURI(url), '_system');
             });
         }
     }
