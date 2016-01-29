e01a559eb66174e29fe9661c27ea7383914d4935
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-01-29 12:34:46 +0100

    Fix map link
    Add a map link directive to open default map app.

--- www/js/directives.js
+++ www/js/directives.js
@@ -5,7 +5,8 @@
     // define the module that will hold all the factories we're gonna use
     angular.module('directives', [])
       .directive('uboIframeOnload', ['$parse', iframeOnloadDirective])
-      .directive('uboHref', anchorDirective);
+      .directive('uboHref', extLinkDirective)
+      .directive('uboGeoLink', geoLinkDirective);
 
     function iframeOnloadDirective($parse) {
         var directive = {
@@ -28,7 +29,7 @@
 
     // Directive to open external links using inAppBrowser
     // Source: https://forum.ionicframework.com/t/how-to-opening-links-in-content-in-system-browser-instead-of-cordova-browser-wrapper/2427/10
-    function anchorDirective() {
+    function extLinkDirective() {
         var directive = {
             link: link,
             restrict: 'A'
@@ -49,6 +50,50 @@
                 e.preventDefault();
                 if(attrs.ngClick){
                     scope.$eval(attrs.ngClick);
+                }
+                window.open(encodeURI(url), '_system');
+            });
+        }
+    }
+
+    // Directive to open map in default map app
+    function geoLinkDirective() {
+        var directive = {
+            link: link,
+            restrict: 'E',
+            template: '<a href="#" ng-transclude></a>',
+            transclude: true,
+            replace: true,
+            scope: '='  // isolate scope
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            var lat = attrs.lat;
+            var lng = attrs.lng;
+            var q = attrs.query;
+
+            // attrs.$observe('lat', function(value) {
+            //     if (value) {
+            //         lat = lat;
+            //     }
+            // });
+
+            element.on('click',function(e){
+                var url,
+                    coords = encodeURIComponent(lat) + ',' + encodeURIComponent(lng),
+                    query;
+
+                e.preventDefault();
+                if(attrs.ngClick){
+                    scope.$eval(attrs.ngClick);
+                }
+                if (ionic.Platform.isIOS()) {
+                    query = 'sll=' + coords + '&q=' + encodeURIComponent(q) + '&z=17';
+                    url = 'maps:?' + query;
+                } else {
+                    query = 'q=' + encodeURIComponent(q) + '&z=16';
+                    url = 'geo:' + coords + '?' + query;
                 }
                 window.open(encodeURI(url), '_system');
             });
