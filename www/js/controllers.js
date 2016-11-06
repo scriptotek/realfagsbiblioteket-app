217f28ae87abeb59bb4f08399424a224b44068e7
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-11-06 15:18:47 +0100

    Update to use latest appinfo plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -50,42 +50,33 @@
         if (!window.navigator.appInfo) {
           vm.status = 'Not currently running on a device.';
         } else {
-          window.navigator.appInfo.getAppInfo(function(appInfo) {
-            // console.log('identifier: %s', appInfo.identifier);
-            // console.log('version: %s', appInfo.version);
-            // console.log('build: %s', appInfo.build);
-            // console.log('platform: %s', device.platform);
+          var platform = ionic.Platform.platform();
+          var deviceInfo = ionic.Platform.device();
+          var appVersion = window.navigator.appInfo.version;
 
-            var platform = ionic.Platform.platform();
-            var deviceInfo = ionic.Platform.device();
-            var appVersion = appInfo.version;
+          vm.status = null;
 
-            vm.status = null;
-
-            console.log('Checking server status...');
-            $http({
-              url: 'https://ub-www01.uio.no/realfagsbiblioteket-app/status',
-              method: 'GET',
-              cache: false,
-              params: {
-                platform: platform,
-                platform_version: deviceInfo.version,
-                app_version: appVersion,
-                device: deviceInfo.manufacturer + ' ' + deviceInfo.model,
-              }
-            }).then(function(response) {
-              if (response.data.status == 'ok') {
-                console.log(' > Server says it\'s OK!');
-              } else {
-                console.log(' > Server reports problems:');
-                console.log(' > ' + response.data.status);
-                vm.status = response.data.status;
-              }
-            }, function(error) {
-              console.log(' > Status message endpoint is unreachable!');
-            });
-          }, function(err) {
-              console.log(err);
+          console.log('Checking server status...');
+          $http({
+            url: 'https://ub-www01.uio.no/realfagsbiblioteket-app/status',
+            method: 'GET',
+            cache: false,
+            params: {
+              platform: platform,
+              platform_version: deviceInfo.version,
+              app_version: appVersion,
+              device: deviceInfo.manufacturer + ' ' + deviceInfo.model,
+            }
+          }).then(function(response) {
+            if (response.data.status == 'ok') {
+              console.log(' > Server says it\'s OK!');
+            } else {
+              console.log(' > Server reports problems:');
+              console.log(' > ' + response.data.status);
+              vm.status = response.data.status;
+            }
+          }, function(error) {
+            console.log(' > Status message endpoint is unreachable!');
           });
         }
       }
