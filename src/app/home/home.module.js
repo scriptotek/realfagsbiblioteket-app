a1083110ae4d9b3159700390ad6b3756951c5109
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-11-10 23:23:12 +0100

    Remove warning about ebook availability
    Should be fixed on server side now.
    
    Also, show server status independently of whether we are on a device or not.

--- src/app/home/home.module.js
+++ src/app/home/home.module.js
@@ -14,39 +14,43 @@
     /////
 
     function ready() {
+      vm.status = null;
+      var device_info,
+        device_str,
+        platform,
+        platform_version,
+        app_version;
 
-      if (!window.navigator.appInfo) {
-        vm.status = 'Not currently running on a device.';
-      } else {
-        var platform = ionic.Platform.platform();
-        var deviceInfo = ionic.Platform.device();
-        var appVersion = window.navigator.appInfo.version;
-
-        vm.status = null;
-
-        console.log('Checking server status...');
-        $http({
-          url: 'https://ub-www01.uio.no/realfagsbiblioteket-app/status',
-          method: 'GET',
-          cache: false,
-          params: {
-            platform: platform,
-            platform_version: deviceInfo.version,
-            app_version: appVersion,
-            device: deviceInfo.manufacturer + ' ' + deviceInfo.model,
-          }
-        }).then(function(response) {
-          if (response.data.status == 'ok') {
-            console.log(' > Server says it\'s OK!');
-          } else {
-            console.log(' > Server reports problems:');
-            console.log(' > ' + response.data.status);
-            vm.status = response.data.status;
-          }
-        }, function() {
-          console.log(' > Status message endpoint is unreachable!');
-        });
+      if (window.navigator.appInfo) {
+        app_version = window.navigator.appInfo.version;
+        platform = ionic.Platform.platform();
+        device_info = ionic.Platform.device();
+        platform_version = device_info.version;
+        device_str = device_info.manufacturer + ' ' + device_info.model;
       }
+
+      console.log('Checking server status...');
+      $http({
+        url: 'https://ub-www01.uio.no/realfagsbiblioteket-app/status',
+        method: 'GET',
+        cache: false,
+        params: {
+          platform: platform,
+          platform_version: platform_version,
+          app_version: app_version,
+          device: device_str,
+        }
+      }).then(function(response) {
+        if (response.data.status == 'ok') {
+          console.log(' > Server says it\'s OK!');
+        } else {
+          console.log(' > Server reports problems:');
+          console.log(' > ' + response.data.status);
+          vm.status = response.data.status;
+        }
+      }, function() {
+        console.log(' > Status message endpoint is unreachable!');
+      });
     }
   }
 
